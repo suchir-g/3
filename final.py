3028c4ef2296da3e15a86ad9ee747789b519293c
suchir-g <18sgupta@heckgrammar.co.uk>
2023-12-08 17:22:15 +0000

    commit

--- final.py
+++ final.py
@@ -1,8 +1,9 @@
+import math
 class Game:
     def __init__(self, teams, players, boards, rounds):
 
-        self.teams = teams
-        self.players = players
+        self.teams = teams # all of them are integers
+        self.players = players 
         self.boards = boards
         self.rounds = rounds
 
@@ -17,7 +18,7 @@ class Game:
 
         totalPeople = teams * players
         self.played = [[0 for i in range(totalPeople)] for j in range(totalPeople)]
-
+        self.teamPlayed = [[0 for i in range(teams)] for j in range(teams)]
         # self.played[0][6] gets the status for A.01 playing B.01
         # we can represent the colour of their matches using this system
         # 0 = not played, 1 = played as white, 2 = played as black
@@ -45,7 +46,7 @@ class Game:
         for brd in self.rep:
             print(brd)
 
-            # the below code is for displaying all the individual pairs (and their indexes)
+            # the below code is for displaying all the individual pairs (and their indexes) 
             # for rnd in brd:
             #     for match in rnd:
             #         white, black = match.split("/")
@@ -55,23 +56,103 @@ class Game:
         # this goes through every board and populate of who has played who.
         # since there can be upfloats and downfloats, we need to check every single game
         # this is stored in the main "played" array using indexing.
+
+        # as well as this, we need to populate the teamPlayed matrix
+        # so we will just check the team index of them
         for brd in self.rep:
             for rnd in brd:
                 for match in rnd:
                     white, black = match.split("/")
                     wIndex, bIndex = self.GrabIndex(white), self.GrabIndex(black)
 
-                    if self.played[wIndex][bIndex] == self.played[bIndex][wIndex] == 0:
-                        self.played[wIndex][bIndex] += 1
-                        self.played[bIndex][wIndex] -= 1
-                    else:
-                        print("This is an INVALID arrangement.")
-                        print("People are playing each other more than once.")
-                        self.valid = False
+                    wTeamIndex, bTeamIndex = wIndex // 6, bIndex // 6 # diving by 6 to make them into team indexes
+
+                    self.played[wIndex][bIndex] += 1
+                    self.played[bIndex][wIndex] -= 1
+
+                    self.teamPlayed[wTeamIndex][bTeamIndex] += 1
+                    self.teamPlayed[bTeamIndex][wTeamIndex] += 1
+                    
+
 
         for row in self.played:
             print(row)
 
+    def TeamPlayedQuota(self):
+        floorVal = math.floor(self.rounds * self.boards / (self.teams - 1))
+        ceilVal = math.ceil(self.rounds * self.boards / (self.teams - 1))
+
+        return (floorVal, ceilVal)
+
+    def checkUpfloats(self):
+        upfloatCount = 0
+        downfloatCount = 0
+        for brdIndex in range(len(self.rep)):
+            for rnd in self.rep[brdIndex]:
+                for match in rnd:
+                    white, black = match.split("/")
+                    
+                    wNumber, bNumber = int(white[-2:]), int(black[-2:]) # extracting the last 2 digits from the numbers
+                    if wNumber - 2 == brdIndex: # commend & explain this later
+                        upfloatCount += 1
+                    if bNumber - 2 == brdIndex:
+                        # upfloats CAN'T be black. Therefore, we can just return false return now
+                        return False
+                    
+                    # now let's handle downfloats
+
+                    if wNumber + 2 == brdIndex or bNumber + 2 == brdIndex:
+                        downfloatCount += 1
+
+            if upfloatCount > 1:
+                return False
+            
+            if downfloatCount > 1:
+                return False 
+
+        return True
+
+
+
+    def CheckValidPlayed(self):
+        # note: the notation [0]*len just means an array filled with 0s, len times
+        allPlayed = [0]*self.teams # initialises an array filled with the number of times a team has played.
+        upfloats = [0]*self.rounds
+        
+        for brd in self.rep:
+            for rnd in brd:
+                for match in rnd:
+                    white, black = match.split("/")
+                    if white[0] == black[0]:
+                        return False
+                    wIndex, bIndex = self.GrabIndex(white), self.grabIndex(black)
+                    wTeamIndex, bTeamIndex = wIndex // 6, bIndex // 6
+
+                    allPlayed[wTeamIndex] += 1
+                    allPlayed[bTeamIndex] += 1
+
+                    if self.played[wIndex][bIndex] not in ("0", "1", "-1"):
+                        return False
+                        
+                roundIndex += 1
+                    
+        # now we have to check if they've hit the quota for team games played.
+
+        quota = self.TeamPlayedQuota()
+
+        for gamesPlayed in allPlayed:
+            if gamesPlayed not in quota:
+                return False
+        
+        # now we will check the amounts of upfloats per round.
+        # so we have to do something similar, with an array with the length of round called upfloats
+        # this will be done in a seperate function - it will return true if all the upfloats/downfloats are valid.
+
+        # the logic for this is in the conditions           
+
+            
+
+                    
         
 
 game = Game(6, 6, 3, 4)
